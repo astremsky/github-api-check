638b2e9f3afb4579eab0010c6971b49a943ed4e2
Athony A <astremsky@gmail.com>
2020-09-12 23:59:12 -0400

    state update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
 
   const [counter, setCounter] = useState(0)
 
+  const [state, setState] = useState({
+    title: 'Some testing title',
+    date: Date.now()
+  })
+
   let increment = () => setCounter(counter+1)
   let decrement = () => setCounter(counter-1)
 
@@ -16,6 +21,7 @@ function App() {
       <div className="buttonGroup">
         <button onClick={increment} className="btn btn-success">+</button>
         <button onClick={decrement} className="btn btn-danger">-</button>
+        <button onClick={decrement} className="btn">Update</button>
       </div>
     </div>
   );
