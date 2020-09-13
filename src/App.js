69f0205706993a3fdd6aa967e11ce22c816ca20b
Athony A <astremsky@gmail.com>
2020-09-12 23:45:26 -0400

    increment/decrement test functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  let increment = () => setCounter(counter+1)
+  let decrement = () => setCounter(counter-1)
+
   return (
     <div className="App">
       <h1>Test Task with hooks</h1>
-      <h2>Counter</h2>
+      <h2>Counter: {counter}</h2>
       <div className="buttonGroup">
-        <button className="btn btn-success">+</button>
-        <button className="btn btn-danger">-</button>
+        <button onClick={increment} className="btn btn-success">+</button>
+        <button onClick={decrement} className="btn btn-danger">-</button>
       </div>
     </div>
   );
