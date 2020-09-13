5f963cf27716f48a3311b1154e2674d2aec222de
Athony A <astremsky@gmail.com>
2020-09-12 22:25:28 -0400

    Button structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Test Task with hooks</h1>
+      <h2>Counter</h2>
+      <div className="buttonGroup">
+        <button className="btn btn-success">+</button>
+        <button className="btn btn-danger">-</button>
+      </div>
     </div>
   );
 }
