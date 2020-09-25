34cf3b5d28b83c320e637e5a9734ff036cfe5c1a
Athony A <astremsky@gmail.com>
2020-09-24 22:52:47 -0400

    Card component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import {Search} from '../components/Search'
+import {Card} from '../components/Card'
+import { id } from 'postcss-selector-parser';
 
 export const Home = () => {
+  const cards = new Array(12)
+    .fill('')
+    .map((_,i) => i)
   return(
-    <div>
+    <Fragment>
       <Search />
-    </div>
+      <div className='row'>
+        
+        {cards.map(card => {
+          return(
+            <div className='col-sm-4 mb-4' key={id.card}>
+          <Card />
+        </div>
+          )
+        })}
+
+      </div>
+    </Fragment>
   )
 }
